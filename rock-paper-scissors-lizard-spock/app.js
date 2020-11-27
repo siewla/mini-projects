cadcf8da951df3b611f647137e2dc2664ace25d6
siewla <siewlapang@gmail.com>
2020-11-27 16:08:16 +0800

    add css

--- rock-paper-scissors-lizard-spock/app.js
+++ rock-paper-scissors-lizard-spock/app.js
@@ -1,3 +1,20 @@
+function ready(callbackFunc) {
+  if (document.readyState !== 'loading') {
+    // Document is already ready, call the callback directly
+    callbackFunc();
+  } else if (document.addEventListener) {
+    // All modern browsers to register DOMContentLoaded
+    document.addEventListener('DOMContentLoaded', callbackFunc);
+  } else {
+    // Old IE browsers
+    document.attachEvent('onreadystatechange', function () {
+      if (document.readyState === 'complete') {
+        callbackFunc();
+      }
+    });
+  }
+}
+
 const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
 const results = [
@@ -22,7 +39,8 @@ const getResults = (userChoice, computerChoice) => {
   userChoice = userChoice.toLowerCase();
   computerChoice = computerChoice.toLowerCase();
   // console.log(userChoice, computerChoice);
-  if (userChoice.toLowerCase() === computerChoice) return 'It is a Tie';
+  if (userChoice.toLowerCase() === computerChoice)
+    return ['It is a Tie', 'Tie'];
 
   const splitResult = results.map((result) => result.split(' '));
 
@@ -31,13 +49,50 @@ const getResults = (userChoice, computerChoice) => {
       splitResult[i][0].toLowerCase() === userChoice &&
       splitResult[i][2].toLowerCase() === computerChoice
     )
-      return [splitResult[i], 'User Wins'];
+      return [splitResult[i].join(' '), 'User Wins'];
     if (
       splitResult[i][0].toLowerCase() === computerChoice &&
       splitResult[i][2].toLowerCase() === userChoice
     )
-      return [splitResult[i], 'Computer Wins'];
+      return [splitResult[i].join(' '), 'Computer Wins'];
   }
 };
 
-console.log(getResults(getRandomChoice(choices), getRandomChoice(choices)));
+// console.log(getResults(getRandomChoice(choices), getRandomChoice(choices)));
+
+const mainGame = () => {
+  const choicesDOM = document.querySelectorAll('.choices');
+  const resultsDiv = document.querySelector('.results');
+
+  choicesDOM.forEach((choice) =>
+    choice.addEventListener('click', (event) => {
+      document.querySelector('.results-container').style.display = 'block';
+      resultsDiv.innerHTML = '';
+      const userMove = event.target.id;
+      const computerMove = getRandomChoice(choices);
+      const results = getResults(userMove, computerMove);
+      const firstResultText = document.createElement('h1');
+      firstResultText.setAttribute('id', 'first-result-text');
+      const secondResultText = document.createElement('h1');
+      secondResultText.setAttribute('id', 'second-result-text');
+      const yourMove = document.createElement('h1');
+      yourMove.setAttribute('id', 'your-move');
+      const opponentMove = document.createElement('h1');
+      opponentMove.setAttribute('id', 'computer-move');
+      firstResultText.innerHTML = results[0];
+      secondResultText.innerHTML = results[1];
+      yourMove.innerHTML = `Your Choice: ${userMove}`;
+      opponentMove.innerHTML = `Computer\'s Choice: ${computerMove}`;
+      resultsDiv.append(
+        yourMove,
+        opponentMove,
+        firstResultText,
+        secondResultText,
+      );
+    }),
+  );
+};
+
+ready(function () {
+  mainGame();
+});
