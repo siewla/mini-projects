7235bfd8e878c816f4eda0c22ac861fa200e091d
siewla <siewlapang@gmail.com>
2020-12-18 14:48:16 +0800

    Commit 1 - I added my first element and it was fun

--- jquery-magic/app.js
+++ jquery-magic/app.js
@@ -3,3 +3,18 @@ if (typeof jQuery == 'undefined') {
 } else {
   console.log('I did it! I linked jQuery and this js file!');
 }
+
+$(() => {
+  /* YEAR 1 */
+
+  // Query for your div with the id of container and set it to a variable named $container
+  // console.log $container
+  const $container = $('#container');
+  console.log($container);
+
+  // Create an <h1> element and set it to a variable called $h1 and console log it
+  // Add some text inside the h1 element. Example text: 'Hogwarts'
+  const $h1 = $('<h1>').text('Hogwarts');
+
+  $container.append($h1);
+});
