e5e667dc70be70f9d0caf58dfa4e6b6292fbf0f1
siewla <siewlapang@gmail.com>
2020-12-18 15:39:08 +0800

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- jquery-magic/app.js
+++ jquery-magic/app.js
@@ -1,8 +1,14 @@
-if (typeof jQuery == 'undefined') {
-  console.log('oops! I still have to link my jQuery properly!');
-} else {
-  console.log('I did it! I linked jQuery and this js file!');
-}
+// if (typeof jQuery == 'undefined') {
+//   console.log('oops! I still have to link my jQuery properly!');
+// } else {
+//   console.log('I did it! I linked jQuery and this js file!');
+// }
+
+const createElementWithText = (type, content) => {
+  return $(`<${type}>`).text(`${content}`);
+};
+
+const houses = ['Hufflepuff', 'Gryffindor', 'Ravenclaw', 'Slytherin'];
 
 $(() => {
   /* YEAR 1 */
@@ -17,4 +23,25 @@ $(() => {
   const $h1 = $('<h1>').text('Hogwarts');
 
   $container.append($h1);
+
+  /* YEAR 2 */
+  // h2 element with your name
+  const $name = createElementWithText('h2', 'Siew La');
+
+  // h3 element with your house
+  // ( )
+  const $house = createElementWithText('h3', houses[1]);
+
+  // h4 element with your pet's name
+  // this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+  const $pet = createElementWithText('h4', 'Hedwig').addClass('Owl');
+
+  // h4 element with your wand
+  const $wand = createElementWithText('h4', 'Elder Wand');
+  // (You can make it whatever you want. Need help coming up with one?
+  // Here is one of many websites to help you find your wand playbuzz.com )
+  $container.append($name, $house, $pet, $wand);
+
+  // In your main.css add an attribute of font-family: fantasy;
+  // to all elements that have a class that matches your pet's class
 });
